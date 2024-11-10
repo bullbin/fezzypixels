0009ce3b48ac36d0695e6cccdd2e6fab6ba047e5
bullbin <34871082+bullbin@users.noreply.github.com>
2024-11-10 16:40:20 +0000

    Add support for JJN diffusion and masking
    Changes
    - Add variance-based mask generation technique to blend diffusion and ordered dithering
    - Add JJN (Bell) diffusion matrix. Similar to Atkinsons but doesn't remove error
    - Add diffusion skip mask support
    
    Bugfixes

--- dither/error_diffuse/diffusion_dither.py
+++ dither/error_diffuse/diffusion_dither.py
@@ -2,10 +2,11 @@ import numpy as np
 from dither.color import lin_srgb_to_srgb, srgb_to_lin_srgb, lin_srgb_to_oklab
 from dither.palette import ColorSimilarityMetric, get_most_similar_shade_index_lab, get_most_similar_shade_index_srgb_binary, get_most_similar_shade_index_srgb_blend
 from .diffusion_weights import get_dither_weighting, DitheringWeightingMode
+from typing import Optional
 
 def error_diffusion_dither_srgb(image_srgb_norm : np.ndarray, palette_srgb : np.ndarray, metric : ColorSimilarityMetric = ColorSimilarityMetric.DELTA_E_FAST,
                                 diffuse_mode : DitheringWeightingMode = DitheringWeightingMode.ATKINSON,
-                                serpentine : bool = True) -> np.ndarray:
+                                serpentine : bool = True, skip_mask : Optional[np.ndarray] = None) -> np.ndarray:
     """Dither an image using error-diffusion dithering.
 
     Args:
@@ -14,10 +15,20 @@ def error_diffusion_dither_srgb(image_srgb_norm : np.ndarray, palette_srgb : np.
         metric (ColorSimilarityMetric, optional): Color similarity metric. RGB is faster, DELTA_E is more perceptual. . Defaults to ColorSimilarityMetric.DELTA_E_FAST.
         diffuse_mode (DitheringWeightingMode, optional): Diffusion matrix to use. Defaults to DitheringWeightingMode.ATKINSON.
         serpentine (bool, optional): Whether to swap diffusion direction per-row. Reduces diffusion artifacts. Defaults to True.
+        skip_mask (np.ndarray, optional): Mask of where to skip error diffusion. Must be same size as input image. True (any value != 0) will skip diffusion on image. None to diffuse everywhere. Defaults to None.
 
     Returns:
         np.ndarray: Quantized image in normalized sRGB color.
     """
+
+    if not(skip_mask is None):
+        assert skip_mask.shape[0] == image_srgb_norm.shape[0] and skip_mask.shape[1] == image_srgb_norm.shape[1]
+
+        skip_mask = skip_mask.astype(np.float32)
+        skip_mask[skip_mask != 0] = 1
+        skip_mask = 1 - skip_mask.astype(np.uint8)
+    else:
+        skip_mask = np.ones((image_srgb_norm.shape[0], image_srgb_norm.shape[1]))
 
     output_srgb = np.zeros(shape=(image_srgb_norm.shape[0], image_srgb_norm.shape[1], 3), dtype=np.float32)
     
@@ -68,6 +79,6 @@ def error_diffusion_dither_srgb(image_srgb_norm : np.ndarray, palette_srgb : np.
                     dest_x = coord[1] + x
 
                 if 0 <= dest_y < image_linear.shape[0] and 0 <= dest_x < image_linear.shape[1]:
-                    image_linear[dest_y,dest_x] += error_linear * weight
-    
+                    image_linear[dest_y,dest_x] += error_linear * weight * skip_mask[dest_y,dest_x]
+
     return output_srgb.astype(np.float32)
